fa1fd106cf96c7b182e6ddfe32ad968799d10117
arnaud_paran <paran.arnaud@gmail.com>
2015-04-22 10:14:04 +0200

    Affichage des graphes

--- chaleur1D.py
+++ chaleur1D.py
@@ -4,6 +4,7 @@
 import numpy as np
 from numpy import linalg
 from valeurs import*
+import matplotlib.pyplot as plt
 
 #définition de la solution
 U=[U0]
@@ -12,10 +13,16 @@ U=[U0]
 A=I(nx)+nu*B(nx)
 
 #nombre d'itérations
-N=10
+N=100
+#on implémente le schéma d'euler
 for n in range(1,N):
 	Un=linalg.solve(A,U[n-1])
 	Un+=Ubord
 	U.append(Un)
 
-
+#on trace la solution
+x=range(0,nx)
+plt.plot(x,U[0][x])
+plt.plot(x,U[N/2][x])
+plt.plot(x,U[N-1][x])
+plt.show()
