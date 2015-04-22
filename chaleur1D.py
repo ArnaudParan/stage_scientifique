8c344e1cd6dc92686d65ff87e3fcbfec78335c2a
arnaud_paran <paran.arnaud@gmail.com>
2015-04-22 09:38:01 +0200

    Version du 21 avril, n'est pas fonctionnelle

--- chaleur1D.py
+++ chaleur1D.py
@@ -2,5 +2,17 @@
 #-*-coding:utf-8-*
 
 import numpy as np
+from numpy import linalg
 from valeurs import*
+
+#définition de la solution
+U=[U0]
+
+#la matrice du schéma à inverser
+A=I(nx)+nu*B(nx)
+
+#nombre d'itérations
+N=10
+for n in range(1,N):
+	U.append(linalg.solve(A,U[n-1]))
 
--- valeurs.py
+++ valeurs.py
@@ -2,7 +2,6 @@
 #-*-coding:utf-8-*
 
 import numpy as np
-import matplotlib.pyplot as plt
 
 def B(n):
 	B=np.diag(np.ones(n-1),1)
@@ -12,3 +11,27 @@ def B(n):
 
 def I(n):
 	return np.identity(n)
+
+#coeff diffusion
+D=10.
+
+#définition du maillage
+nx=10
+dx=0.1
+dt=0.1
+
+#condition initiale intérieure
+U0=np.array([0.]*nx)
+
+#conditions de bord ici, on fixe u à 10 sur le bord
+Ubord=np.array([0.]*nx)
+Ubord[0]=10.
+Ubord[nx-1]=10.
+
+
+U0+=Ubord
+
+
+
+#grandeurs relatives à l'équation
+nu=D*dt/(dx*dx)
