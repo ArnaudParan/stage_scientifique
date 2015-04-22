b63b819d2b208ebf413999132eb11cbae25960a3
arnaud_paran <paran.arnaud@gmail.com>
2015-04-22 10:22:18 +0200

    changement de la méthode d'affichage

--- chaleur1D.py
+++ chaleur1D.py
@@ -13,7 +13,9 @@ U=[U0]
 A=I(nx)+nu*B(nx)
 
 #nombre d'itérations
-N=100
+tmax=10
+N=int(tmax/dt)
+print(N)
 #on implémente le schéma d'euler
 for n in range(1,N):
 	Un=linalg.solve(A,U[n-1])
@@ -22,7 +24,8 @@ for n in range(1,N):
 
 #on trace la solution
 x=range(0,nx)
-plt.plot(x,U[0][x])
-plt.plot(x,U[N/2][x])
-plt.plot(x,U[N-1][x])
-plt.show()
+#nombre d'affichages
+Naff=5
+for n in range(0,Naff-1):
+	plt.plot(x,U[n*N/Naff][x])
+	plt.show()
